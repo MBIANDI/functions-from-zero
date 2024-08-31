3edda7782eac91832ee477d5536d822b07e79567
MBIANDI <mbialaura12@gmail.com>
2024-08-31 23:14:31 +0000

    delete myStatements.py

--- myStatements.py
+++
@@ -1,8 +0,0 @@
-my_list = ["apple", "cherry", "lemon"]
-my_dict = {"drink": "coffee", "laura": "leo"}
-
-for item in my_list:
-    print(f"My favourite thing to eat is {item}")
-
-for key, value in my_dict.items():
-    print(f"My favorite {key} is {value}")
